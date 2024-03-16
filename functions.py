ea0b6b68dcedc97b254b7385c9e578c8a93dc176
sn20233 <sn2023@hw.ac.uk>
2024-03-16 11:22:25 +0400

    removed statement

--- functions.py
+++ functions.py
@@ -9,19 +9,19 @@ def preprocess(text):
     text = [''.join([char if char.isalnum() else ' ' for char in word]) for word in text.split()]
     text = ' '.join(text)
      # used word_tokenize function to tokenize the text, gives list
-    if text!= NaN:
-        tokenized_text = word_tokenize(text.lower())
-        # get the stop words
-        stop_words = set(stopwords.words('english'))
-        # removed stop words
-        tokenized_text = [word for word in tokenized_text if word not in stop_words]
-        # applying stemming 
-        stemmer = PorterStemmer()
-        tokenized_text = [stemmer.stem(word) for word in tokenized_text]
-        # applying lemmatization
-        lemmatizer = WordNetLemmatizer()
-        tokenized_text = [lemmatizer.lemmatize(word) for word in tokenized_text]
+ 
+    tokenized_text = word_tokenize(text.lower())
+    # get the stop words
+    stop_words = set(stopwords.words('english'))
+    # removed stop words
+    tokenized_text = [word for word in tokenized_text if word not in stop_words]
+    # applying stemming 
+    stemmer = PorterStemmer()
+    tokenized_text = [stemmer.stem(word) for word in tokenized_text]
+    # applying lemmatization
+    lemmatizer = WordNetLemmatizer()
+    tokenized_text = [lemmatizer.lemmatize(word) for word in tokenized_text]
 
-        preprocessed_text = ' '.join(tokenized_text)
+    preprocessed_text = ' '.join(tokenized_text)
     
     return preprocessed_text
